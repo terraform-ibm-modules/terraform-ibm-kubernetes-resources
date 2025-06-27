d40d328bcd599abbc9a0dcda6ee451bbb410eb68
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-06-27 13:41:49 +0530

    changed output name

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -2,7 +2,7 @@ data "kubernetes_all_namespaces" "allns" {
   depends_on = [module.namespaces]
 }
 
-output "my_namespace_present" {
+output "all_namespaces" {
   value       = data.kubernetes_all_namespaces.allns.namespaces
   description = "Returns all namespaces"
 }
